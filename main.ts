b21250493ddab55d275a53c45a8890e432d0a7c2
junwoo <96633366+kobukmeat@users.noreply.github.com>
2022-11-29 09:33:53 +0900

    MakeCode 프로젝트용 기초 파일

---
+++ main.ts
@@ -0,0 +1,66 @@
+input.onGesture(Gesture.TiltRight, function () {
+	
+})
+input.onPinPressed(TouchPin.P0, function () {
+	
+})
+input.onGesture(Gesture.TiltLeft, function () {
+	
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+	
+})
+music.onEvent(MusicEvent.MelodyNotePlayed, function () {
+	
+})
+input.onGesture(Gesture.ScreenUp, function () {
+	
+})
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onGesture(Gesture.SixG, function () {
+	
+})
+input.onGesture(Gesture.FreeFall, function () {
+	
+})
+input.onSound(DetectedSound.Loud, function () {
+	
+})
+input.onGesture(Gesture.LogoDown, function () {
+	
+})
+input.onGesture(Gesture.Shake, function () {
+	
+})
+input.onGesture(Gesture.ScreenDown, function () {
+	
+})
+input.onLogoEvent(TouchButtonEvent.Released, function () {
+	
+})
+input.onGesture(Gesture.ThreeG, function () {
+	
+})
+input.onGesture(Gesture.EightG, function () {
+	
+})
+input.onButtonPressed(Button.AB, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+input.onGesture(Gesture.LogoUp, function () {
+	
+})
+input.onLogoEvent(TouchButtonEvent.Touched, function () {
+	
+})
+input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
+	
+})
+basic.forever(function () {
+	
+})
